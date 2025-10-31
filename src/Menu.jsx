b14063dbae7467bfc5969a5b87e79760002129a2
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2025-10-31 16:02:15 +0100

    Avanço do hamburguer

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,14 +1,69 @@
 import Hamburger from "hamburger-react";
 import { useState } from "react";
 
-// let linhas = ["Home", "Contato", "Serviços", "Reclame aqui"];
+let pages = [
+  {
+    title: "Início",
+    path: "/",
+    color: "primaria",
+  },
+  {
+    title: "Residencial",
+    path: "/residencial",
+    color: "primaria",
+  },
+  {
+    title: "Corporativo",
+    path: "/corporativo",
+    color: "primaria",
+  },
+  {
+    title: "Contato",
+    path: "/contato",
+    color: "primaria",
+  },
+  {
+    title: "Área do Cliente",
+    path: "/cliente",
+    color: "secundaria",
+  },
+  {
+    title: "Contratos",
+    path: "/contratos",
+    color: "secundaria",
+  },
+  {
+    title: "Speed Teste",
+    path: "/speedtest",
+    color: "secundaria",
+  },
+  {
+    title: "SoftPhone",
+    path: "/softphone",
+    color: "secundaria",
+  },
+  {
+    title: "Suporte",
+    path: "/suporte",
+    color: "secundaria",
+  },
+];
 
 function Menu() {
   const [open, setOpen] = useState(false);
   return (
-    <div className="bg-blue-400">
-      <Hamburger size={28} toggled={open} toggle={setOpen} />;
-      {open && <div className="bg-blue-500">menu </div>}
+    <div className="bg-red-400 justify-items-center">
+      <p className="text-xs">menu</p>
+      <Hamburger size={28} toggled={open} toggle={setOpen} />
+      {open && (
+        <div className="bg-blue-500">
+          <ul className="md:flex">
+            {pages.map((page, index) => (
+              <li key={index}>{page.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
